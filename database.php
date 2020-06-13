117cbc4d2b6fec512ac16c0149f96700c962a2b9
Bhuvesh Aggarwal <bhuveshaggarwal@gmail.com>
2020-06-13 12:50:45 +0530

    PHP Script for Database Connection.

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+$hostname="localhost";
+$username="root";
+$password="";
+$dbname="mukul2018";
+$conn=mysqli_connect($hostname,$username,$password,$dbname) or die(mysqli_error());
+//echo "connected";
+?>
